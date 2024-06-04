3641446b4f6fea883e68a46845c70fb4f53ae562
JP1005YT <godlolpro32@gmail.com>
2024-06-04 17:41:36 -0300

    pronto

--- backend/functions/entrarUsuario.php
+++ backend/functions/entrarUsuario.php
@@ -21,6 +21,8 @@
             
             
                     header('Location: ../../index.php');
+                }else{
+                    echo "error";
                 }
             }
         } else {
--- pages/Aula/index.php
+++ pages/Aula/index.php
@@ -1,5 +1,9 @@
 <?php
 $json = json_decode(file_get_contents('../../backend/temas/'.$_GET['class']),true);
+spl_autoload_register(function ($class_name) {
+    include '../../backend/classes/' . $class_name . '.php';
+});
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -13,13 +17,61 @@ $json = json_decode(file_get_contents('../../backend/temas/'.$_GET['class']),tru
         <link rel="shortcut icon" href="./../../logo.png" />
     </head>
     <body>
+        <header class="mainHeader">
+            <section class="logo">
+                <img src="../../logo.png" height="70px">
+                <h1>DataStruct School</h1>
+            </section>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="../../index.php">Página Principal</a>
+                    </li>
+                    <li>
+                        <a href="../../pages/Temas/">Temas</a>
+                    </li>
+                </ul>
+                <?php
+                    if(isset($_SESSION['user'])){
+                        $user = $_SESSION['user'];
+                        echo '<section class="logged" onclick="dropdown()">
+                                <section class="content">
+                                    '.$user->getUsername() .'
+                                    <i class="bx bxs-chevron-down" id="icon"></i>
+                                </section>
+                                <div class="dropdown">
+                                    <ul>
+                                        <li>Perfil</li>
+                                        
+                                        <li>
+                                            <a href="../../backend/functions/sair.php">Sair</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </section>';
+                    }else{
+                        echo '<section>
+                                <button class="cadastrar" onclick="switchPages('."'../../pages/Cadastrar/'".')">Cadastrar-se</button>
+                                <button class="entrar" onclick="switchPages('."'../../pages/Entrar/'".')">Entrar</button>
+                            </section>';
+                    }
+                ?>
+            </nav>
+        </header>
         <main>
         <?php
             echo $json['content'];
         ?>
         </main>
         <script>
-            document.title = `DataStruct | ${}`
+            function switchPages(url){
+                window.location.href = url
+            }
+            document.title = `DataStruct | <?php echo $json['nome']?>`
+            function dropdown(){
+            document.querySelector(".logged").classList.toggle("active")
+            document.querySelector("#icon").classList.toggle("active")
+            }
         </script>
     </body>
 </html>
